ddce2f586230bbc034d3c0fee1ee843e407b3e78
Aryan Neupane <152203991+aryan-np@users.noreply.github.com>
2024-05-11 07:22:06 +0545

    local storage js

--- weather script.js
+++ weather script.js
@@ -1,19 +1,26 @@
-const apiKey='your api key'
+const apiKey='5887190f62cc29dd352edfe4e4785801'
 const defaultCity="Highland"
 async function fetchData(cityName){
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+    
     try {
-        const response=await fetch(url)  //fetching ma take some time so using await
-        if (!response.ok) { //.ok returns true boolean value when fetched successfully
-            throw new Error("Error While Fetching data")
+        if (navigator.onLine) {
+            // console.log("gbregr");
+            const response = await fetch(`connection.php?q=${cityName}`);  //fetching ma take some time so using await
+            if (!response.ok) { //.ok returns true boolean value when fetched successfully
+                throw new Error("Error While Fetching data")
+            }
+            let data= await response.json() //converting data into json format
+            console.log(data);
+            updateWeather(data)
+            localStorage.setItem(cityName,JSON.stringify(data));
         }
-        const data= await response.json() //converting data into json format
-        console.log(data);
-        updateWeather(data)
-    } catch (err) {
-        console.log('ERROR'+ err); 
+    else{
+        data=JSON.parse(localStorage.getItem(cityName));
     }
 }
+catch(err){
+console.log("Error Has Occured"+err);
+}}
 // selecting and storing dynamic contents in variable
 const cityElement = document.querySelector(".city");
 const temperature = document.querySelector(".temperature");
@@ -24,15 +31,15 @@ const date = document.querySelector(".date");
 const Icon = document.querySelector(".icon");
 //function to update weather data takes json data from the api as parameters
 function updateWeather(data) {
-    cityElement.textContent = data.name;
+    cityElement.textContent = data[0].city;
     const currentDate = new Date();
     date.textContent = currentDate.toDateString(); //.toDateString gives only date excluding the GMT
-    Icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-                      <span class="description-text">${data.weather[0].description}</span>`
-    temperature.textContent = `${Math.round(data.main.temp)}°C`;
-    pressure.textContent= `${data.main.pressure}`;
-    windSpeed.textContent = `${data.wind.speed} km/h`;
-    humidity.textContent = `${data.main.humidity}%`;
+    Icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data[0].icon_code}@2x.png" alt="">
+                      <span class="description-text">${data[0].weather_description}</span>`
+    temperature.textContent = `${data[0].temperature}°C`;
+    pressure.textContent= `${data[0].pressure}`;
+    windSpeed.textContent = `${data[0].wind} km/h`;
+    humidity.textContent = `${data[0].humidity}%`;
 }
 // selecting and storing search box and search button
 const searchButton = document.querySelector(".search-button");
@@ -45,4 +52,7 @@ searchButton.addEventListener("click", ()=> { //for fetching the data of city ty
         input.value = "";
     }
 });
-fetchData(defaultCity) //Default city
+fetchData(defaultCity) //Default 
+// } catch (err) {
+    //     console.log('ERROR   '+ err); 
+    // }
